8a03308636fc6f831071957071a520646bf4620d
ZahraPayandeh <zahra.payandeh.noujehdehsadat@gu.se>
2024-12-07 21:39:12 +0330

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,7 +1,7 @@
 # Import the pandas library for data manipulation
 import pandas as pd
 # Read data from a CSV file into a pandas DataFrame
-data = pd.read_csv("brca_cnvs_tcga-1-2.csv")
+data = pd.read_csv("brca_cnvs_tcga.csv")
 # Add a new column 'length' which is calculated as loc.end - loc.start
 # This represents the length of the gene
 data['length'] = data['loc.end'] - data['loc.start']
