a7b6688c3cd65b6ceb5536444b2b9184d03ed1c1
ZahraPayandeh <zahra.payandeh.noujehdehsadat@gu.se>
2024-12-11 17:24:25 +0330

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -5,4 +5,7 @@ data = pd.read_csv("brca_cnvs_tcga.csv")
 # Add a new column 'length' which is calculated as loc.end - loc.start
 # This represents the length of the gene
 data['length'] = data['loc.end'] - data['loc.start']
-new_data = data
+# Write the output to a new file
+output_file = 'output.csv'
+data.to_csv(output_file, index=False)
+print(f"Output written to {output_file}")
